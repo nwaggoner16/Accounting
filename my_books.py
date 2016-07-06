e249eb942e13e54c98d7a0b3117aa3d762ddf116
nwaggoner16 <nwaggoner16@gmail.com>
2016-07-05 20:38:43 -0400

    Commit my_books.py

---
+++ my_books.py
@@ -0,0 +1,22 @@
+#Create a dictionary to hold expense information and functions to recall specific info.
+
+from datetime import date
+
+#Create a  class to define accounts
+class account(object):
+	def __init__(self, account, amount, details, date):
+		self.name = name
+		self.account = account
+		self.amount = amount
+		self.details = details
+		self.date = date
+	
+
+def entry(account):
+	one.account = raw_input("What is your account number?")
+	one.amount = raw_input("How much did it cost?")
+	one.details = raw_input("Why did you spend my money?")
+	one.date = raw_input("What is the date?")
+	print account.one
+	
+	
