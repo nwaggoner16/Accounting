2a69cf990301d060c3ee3c87255bf814eade5606
nwaggoner16 <nwaggoner16@gmail.com>
2016-07-05 23:33:47 -0400

    Added write to file feature

--- my_books.py
+++ my_books.py
@@ -14,10 +14,15 @@ class account(object):
 def make_entry():
 	entry = account()
 	print "So you spent %s for %s on %s and you want to put it into account %s?" % (entry.amount, entry.details, entry.date, entry.account_num)
+	mk_str = ("%s, %s, %s, %s") % (entry.account_num, entry.amount, entry.details, entry.date) 
+	with open("acc.csv", "w") as transfer:
+		transfer.write(mk_str)
+		
 
 print "Welcome to Nathan's super awesome accounting program!"
 choice = raw_input("What would you like to do? \nE - Make entry \nV - View account \nX - Exit\n: ") 
 if choice.upper() == "E":
 	make_entry()
+	
 else:
 	print "goodbye"
